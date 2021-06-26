20c8e9967ffb8d7fc8b3b70340a263d49c6bffdc
johaneriksson <joerikss@student.chalmers.se>
2021-06-26 21:13:28 +0200

    Added conditions for differnt sensor values.

--- breath/breath.ino
+++ breath/breath.ino
@@ -2,7 +2,7 @@
 PololuHD44780 lcd(7, 6, 5, 4, 3, 2); 
 
 const int MQ3_PIN = 0;  // Pin for the alcohol sensor.
-int alk_value;          // Value from the alcohol sensor.
+int sensor_value;       // Value from the alcohol sensor.
 
 void setup() {
   Serial.begin(9600);
@@ -12,10 +12,25 @@ void setup() {
 }
 
 void loop() {
-  alk_value = analogRead(MQ3_PIN);
-  Serial.println(alk_value);
+  sensor_value = analogRead(MQ3_PIN);
   
-  lcd.print(alk_value);
+  Serial.println(sensor_value);
+  
+  if (sensor_value < 120) {
+    lcd.print(sensor_value);
+    lcd.gotoXY(0, 1);
+    lcd.print("Nykter");
+  }
+  else if (sensor_value > 120 && sensor_value < 400 ){
+    lcd.print(sensor_value);
+    lcd.gotoXY(0, 1);
+    lcd.print("Booring");
+  }
+  else if (sensor_value > 400) {
+    lcd.print(sensor_value);
+    lcd.gotoXY(0, 1);
+    lcd.print("DRUNK");
+  }
   
   delay(1000);
 }
