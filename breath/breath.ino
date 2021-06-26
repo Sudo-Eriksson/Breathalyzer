3fec18895fa10437b53a939bd9da368234ce7245
johaneriksson <joerikss@student.chalmers.se>
2021-06-26 21:02:48 +0200

    Added LCD support.

--- breath/breath.ino
+++ breath/breath.ino
@@ -1,13 +1,21 @@
+#include <PololuHD44780.h>
+PololuHD44780 lcd(7, 6, 5, 4, 3, 2); 
+
 const int MQ3_PIN = 0;  // Pin for the alcohol sensor.
 int alk_value;          // Value from the alcohol sensor.
 
 void setup() {
   Serial.begin(9600);
   pinMode(MQ3_PIN, INPUT);
+
+  lcd.clear();
 }
 
 void loop() {
   alk_value = analogRead(MQ3_PIN);
   Serial.println(alk_value);
+  
+  lcd.print(alk_value);
+  
   delay(1000);
 }
