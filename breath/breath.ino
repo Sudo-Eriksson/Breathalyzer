3fd6f84a9d5122966b15fed9184ebc440c5975a1
johaneriksson <joerikss@student.chalmers.se>
2021-06-27 19:33:53 +0200

    Added warming time and average measurements.

--- breath/breath.ino
+++ breath/breath.ino
@@ -3,19 +3,30 @@ PololuHD44780 lcd(7, 6, 5, 4, 3, 2);
 
 const int MQ3_PIN = 0;  // Pin for the alcohol sensor.
 int sensor_value;       // Value from the alcohol sensor.
+int loops = 20;
 
 void setup() {
   Serial.begin(9600);
   pinMode(MQ3_PIN, INPUT);
 
   lcd.clear();
+  lcd.print("Warming");
+  delay(10000);
+  lcd.print("Ready")
+  
 }
 
 void loop() {
-  sensor_value = analogRead(MQ3_PIN);
+  sensor_value = 0;
+  
+  for(int i=0; i < loops ; i++){
+    sensor_value += analogRead(MQ3_PIN);
+  }
+
+  sensor_value = sensor_value/loops;
   
   Serial.println(sensor_value);
-  
+    
   if (sensor_value < 120) {
     lcd.print(sensor_value);
     lcd.gotoXY(0, 1);
