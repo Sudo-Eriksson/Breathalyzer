677b2805dcf4544561315ea233fa6d3d397c8159
johaneriksson <joerikss@student.chalmers.se>
2021-07-03 15:42:03 +0200

    Clean up.

--- breath/breath.ino
+++ breath/breath.ino
@@ -1,52 +1,34 @@
-#include <PololuHD44780.h>
-PololuHD44780 lcd(7, 6, 5, 4, 3, 2); 
-
-const int MQ3_PIN = 0;  // Pin for the alcohol sensor.
+const int MQ3_PIN = A5;  // Pin for the alcohol sensor.
 int sensor_value;       // Value from the alcohol sensor.
 int loops = 20;
 
 void setup() {
   Serial.begin(9600);
   pinMode(MQ3_PIN, INPUT);
-
-  lcd.clear();
-  lcd.print("Warming");
+  Serial.println("Setup started");
+  Serial.println("Warming up sensor...");
   delay(10000);
-  lcd.print("Ready");
-  
+  Serial.println("Setup done!");
 }
 
-void loop() {
-  sensor_value = 0;
-  
-  for(int i=0; i < loops ; i++){
-    sensor_value += analogRead(MQ3_PIN);
-  }
-
-  sensor_value = sensor_value/loops;
+void loop() {  
+  sensor_value = analogRead(MQ3_PIN);
   
   Serial.println(sensor_value);
     
   if (sensor_value < 120) {
-    lcd.print(sensor_value);
-    lcd.gotoXY(0, 1);
-    lcd.print("Nykter");
+    // LCD = NYKTER
   }
   else if (sensor_value > 120 && sensor_value < 400 ){
-    lcd.print(sensor_value);
-    lcd.gotoXY(0, 1);
-    lcd.print("Booring");
+    // LCD= Booring
   }
   else if (sensor_value > 400) {
-    lcd.print(sensor_value);
-    lcd.gotoXY(0, 1);
-    lcd.print("DRUNK");
+    //LCD = DRUNK
   }
   
-  delay(1000);
+  delay(200);
 }
 
 int reading_to_BAC(int reading){
   // Convert MQ-3 reading to BAC.
-  
   }
